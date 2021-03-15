8b9cbfba2ebaed8acd2567bee88e3afd84940dd5
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2021-03-15 17:06:41 +0800

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -19,9 +19,6 @@ memory = {}
 
 class response:
     def __init__(self, output, memory):
-        #User Information
-        
-        
         #Declare attributes
         self.message = output['entry'][0]['messaging'][0]
         self.memory = memory
@@ -48,13 +45,11 @@ class response:
         '''Stops message spam'''
         with open('message.pickle', 'wb') as x:
             pickle.dump(self.memory, x, protocol=pickle.HIGHEST_PROTOCOL)   
-
         with open('message.pickle', 'rb') as x:
             past_memory = pickle.load(x)
         if past_memory.get(self.uid):
             if self.message == past_memory[self.uid]:
                 return 'Message Processed'
-        
         self.memory[self.uid] = self.message
         
     def send_message(self):
@@ -62,10 +57,17 @@ class response:
         bot.send_quick_replies(self.uid,self.reply,self.dict)
         return "Message Processed"
 
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error 
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'    
+
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
-
+    
     global memory
 
     if request.method == 'GET':
@@ -84,37 +86,3 @@ def receive_message():
         message = response(output,memory)
         processor(message)
         return "Message Processed"
-
-def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'    
-
-# def feedback(recipient_id):
-#     '''Send a feedback button to let them provide feedback'''
-#     if random.random() <= 0.3:
-#         time.sleep(1.5)
-#         message = 'Thank you for using DEAN! If you have any comments or suggestions, let us know here! :)'
-#         buttons = [
-#                         {
-#                             "type":"postback",
-#                             "title":"Feedback",
-#                             "payload":recipient_id
-#                         }
-#                     ]
-#         button_message(recipient_id,message,buttons)
-#     return "success"
-
-def timer(func):
-    '''Print the Runtime of the decorated function'''
-    @functools.wraps(func)
-    def wrapper_timer(*args,**kwargs):
-        start_time = time.perf_counter()
-        value = func(*args,**kwargs)
-        end_time = time.perf_counter()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
-        return value
-    return wrapper_timer
